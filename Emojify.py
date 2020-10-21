ca0d3a2a9aac1dfa62951cd8f9cebad5da122ba7
KARTIK-z <mishrakartik9525@gmail.com>
2020-10-21 23:02:19 +0530

    version 1.1

--- Emojify.py
+++ Emojify.py
@@ -1,5 +1,20 @@
-from tkinter import Tk, IntVar, Entry, Label, Radiobutton, W, Button, END
-import emoji
+import sys
+import subprocess
+
+
+try:
+    from tkinter import Tk, IntVar, Entry, Label, Radiobutton, W, Button, END
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tk'])
+    from tkinter import Tk, IntVar, Entry, Label, Radiobutton, W, Button, END
+
+
+try:
+    import emoji
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'emoji'])
+    import emoji
+
 
 class class_storage:
     def __init__(self):
